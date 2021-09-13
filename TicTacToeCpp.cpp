f01447840d433ed1561611e3aa3867e89fb7dece
creme332 <65414576+creme332@users.noreply.github.com>
2021-09-13 11:28:30 +0400

    Update TicTacToeCpp.cpp

--- TicTacToeCpp.cpp
+++ TicTacToeCpp.cpp
@@ -1,10 +1,4 @@
-// TicTacToeCpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-#define ll long long
 #include <iostream>
-
-//#include <utility>
-#include <vector>
 using namespace std;
 
 const char emptyspot = '.';
@@ -78,14 +72,3 @@ int main()
     }
  
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
