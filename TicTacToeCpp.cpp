e5ed139f2ff93c42a7bab6594f82f4b953ced3fa
creme332 <65414576+creme332@users.noreply.github.com>
2022-01-29 12:11:32 +0400

    Updated formatting

--- TicTacToeCpp.cpp
+++ TicTacToeCpp.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h> //used to clear terminal screen each round
 using namespace std;
 
 const char emptyspot = '.';
@@ -12,6 +13,8 @@ char board[3][3] = {
     {emptyspot,emptyspot,emptyspot}
 };
 void outputboard() {
+    system("CLS"); //clear previous board from terminal
+    cout << "Rules: Use format <row><space><column> where row, column are 1-3" << endl;
     cout << endl;
     for (int row = 0;row < 3;row++) {
         for (int col = 0;col < 3;col++) {
@@ -33,21 +36,12 @@ char winner() { //checks for winner
     ocount = 0;
     for (int row = 0;row < 4;row++) {
         for (int col = 0;col < 4;col++) {
-            if (board[row][col] == x) {
-                xcount++;
-            }
-            if (board[row][col] == o) {
-                ocount++;
-            }
+            if (board[row][col] == x) {xcount++;}
+            if (board[row][col] == o) {ocount++;}
         }
-        if (xcount == 3) {
-            return x;
-        }
-        if (ocount == 3) {
-            return o;
-        }
-        xcount = 0;
-        ocount = 0;
+        if (xcount == 3) {return x;}
+        if (ocount == 3) {return o;}
+        xcount = 0; ocount = 0;
     }
 
     //vertical check
@@ -55,21 +49,12 @@ char winner() { //checks for winner
     ocount = 0;
     for (int col = 0;col < 4;col++) {
         for (int row = 0;row < 4;row++) {
-            if (board[row][col] == x) {
-                xcount++;
-            }
-            if (board[row][col] == o) {
-                ocount++;
-            }
+            if (board[row][col] == x) {xcount++;}
+            if (board[row][col] == o) {ocount++;}
         }
-        if (xcount == 3) {
-            return x;
-        }
-        if (ocount == 3) {
-            return o;
-        }
-        xcount = 0;
-        ocount = 0;
+        if (xcount == 3) {return x;}
+        if (ocount == 3) {return o;}
+        xcount = 0; ocount = 0;
     }
 
     //positive diagonal check
@@ -90,7 +75,7 @@ char winner() { //checks for winner
     return ongoing;
 }
 void updateboard(bool turn, int row, int col) {
-    //coordinates are valid
+    //coordinates have already been validated
     if (turn == 0) {
         board[row-1][col-1] = o;    // row n has index n-1 and column k has index k-1
     }
@@ -107,18 +92,11 @@ int main()
     bool turnX = 0;// 0 - player o  1 - player x
     char state=ongoing;
     int row, col;
-    cout << "Rules :" << endl;
-    cout << "Enter row <space> column" << endl;
-    cout << "Values of row and column should be between 1 and 3 inclusive" << endl;
 
     while (state==ongoing) {
         outputboard();
-        if (turnX == 0) {
-            cout << "O's turn" << endl;
-        }
-        else {
-            cout << "X's turn" << endl;
-        }
+        if (turnX == 0) {cout << "O's turn" << endl;}
+        else {cout << "X's turn" << endl;}
         cin >> row >> col;
 
         if (validatecoordinates(row, col) == 0) { //invalid coordinates
@@ -133,17 +111,10 @@ int main()
     }
 
     outputboard();
-    if (state == o) {
-        cout << "Player O has won!" << endl;
-    }
+    if (state == o) {cout << "Player O has won!" << endl;}
     else {
-        if (state == x) {
-            cout << "Player X has won!" << endl;
-
-        }
-        else {
-            cout << "Draw" << endl;
-        }
+        if (state == x) {cout << "Player X has won!" << endl;}
+        else {cout << "Draw" << endl;}
     }
  
 }
