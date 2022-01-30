a90aacf0bed3a224260e42313d355676934dfff5
creme332 <65414576+creme332@users.noreply.github.com>
2022-01-30 18:19:39 +0400

    syntactic sugar update + testing winner()

--- TicTacToeCpp.cpp
+++ TicTacToeCpp.cpp
@@ -8,9 +8,9 @@ const char o = 'O';
 const char ongoing = 'n'; //game state = ongoing if no winner yet and board not full
 
 char board[3][3] = { 
-    {emptyspot,emptyspot,emptyspot},
-    {emptyspot,emptyspot,emptyspot},
-    {emptyspot,emptyspot,emptyspot}
+    {emptyspot,emptyspot,o},
+    {emptyspot,emptyspot,x},
+    {x,emptyspot,o}
 };
 void outputboard() {
     system("CLS"); //clear previous board from terminal
@@ -38,11 +38,9 @@ char winner() { //checks for winner
     // "n" if game is ongoing
     // "d" if draw 
 
-    int xcount, ocount; //counts number of x and o
+    int xcount=0, ocount=0; //counts number of x and o
 
     //horizontal check
-    xcount = 0;
-    ocount = 0;
     for (int row = 0;row < 4;row++) {
         for (int col = 0;col < 4;col++) {
             if (board[row][col] == x) {xcount++;}
@@ -54,8 +52,7 @@ char winner() { //checks for winner
     }
 
     //vertical check
-    xcount = 0;
-    ocount = 0;
+    xcount = 0; ocount = 0;
     for (int col = 0;col < 4;col++) {
         for (int row = 0;row < 4;row++) {
             if (board[row][col] == x) {xcount++;}
@@ -85,19 +82,15 @@ char winner() { //checks for winner
     //check if board has an emptyspot => can keep playing
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            if(board[i][j]==emptyspot)return ongoing;
+            if(board[i][j] == emptyspot)return ongoing;
         }
     }
-    return 'd';
+    return 'd'; // draw
 }
 void updateboard(bool turn, int row, int col) {
     //coordinates have already been validated
-    if (turn == 0) {
-        board[row-1][col-1] = o;    // row n has index n-1 and column k has index k-1
-    }
-    else {
-        board[row-1][col-1] = x;
-    }
+    // row n has index n-1 and column k has index k-1
+    board[row-1][col-1] = (turn == 0)? o : x;
 }
 bool validatecoordinates(int row, int col) {
     if (row > 0 && row < 4 && col>0 && col < 4 && board[row-1][col-1]==emptyspot)return 1;
@@ -107,13 +100,12 @@ int main()
 {
     //MUST TEST winner() throughly. There's a bug.
     bool turnX = 0;// 0 - player o  1 - player x
-    char state=ongoing;
+    char state = ongoing;
     int row, col;
 
-    while (state==ongoing) {
+    while (state == ongoing) {
         outputboard();
-        if (turnX == 0) {cout << "O's turn" << endl;}
-        else {cout << "X's turn" << endl;}
+        cout<< ((turnX==0)? "O's turn" : "X's turn") << endl;
         cin >> row >> col;
 
         while(validatecoordinates(row, col) == 0){
@@ -129,8 +121,7 @@ int main()
     outputboard();
     if (state == o) {cout << "Player O has won!" << endl;}
     else {
-        if (state == x) {cout << "Player X has won!" << endl;}
-        else {cout << "Draw" << endl;}
+        cout<< ((state == x)? "Player X has won!" : "Draw" )<< endl;
     }
  
 }
