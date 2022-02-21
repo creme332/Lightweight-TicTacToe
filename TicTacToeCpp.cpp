89f3d90096c0bfa32e475dd8a9b234567f5638a1
creme332 <65414576+creme332@users.noreply.github.com>
2022-02-21 12:54:09 +0400

    minimax now works.

--- TicTacToeCpp.cpp
+++ TicTacToeCpp.cpp
@@ -3,15 +3,18 @@
 #include <stdlib.h> //used to clear terminal screen each round
 using namespace std;
 
-const char emptyspot = '.'; const char x = 'X'; const char o = 'O';
+const char null = '.'; const char x = 'X'; const char o = 'O';
 const char ongoing = 'n'; //game state = ongoing if no winner yet and board not full
 
 vector<vector<char>> board = {
-    {emptyspot,emptyspot,emptyspot},
-    {emptyspot,emptyspot,emptyspot},
-    {emptyspot,emptyspot,emptyspot}
+    {null,null,null},
+    {null,null,null},
+    {null,null,null},
 };
-
+bool IsValid(int row, int col) { //validate user input
+    if (row > 0 && row < 4 && col>0 && col < 4 && board[row - 1][col - 1] == null)return 1;
+    return 0;
+}
 void OutputBoard() {
     system("CLS"); //clear previous board from terminal
     cout << "Use format <row><space><column> where row, column are 1-3" << endl;
@@ -37,22 +40,22 @@ void OutputBoard() {
 };
 char GameState() {
     // returns "X" if x won and "O" if o won 
-    // returns "n" if game is ongoing - game has empty spots
+    // returns "n" if there's no winner and board still has empty spots
     // returns "d" if draw 
-    char k[2] = { x,o };
+    char player[2] = { x,o };
     bool HasEmptySpot = 0;
 
     for (int i = 0;i < 2;i++) {//for each player
         int count[4] = { 0,0,0,0 }; //count along each direction
         for (int r = 0;r < 3;r++) {
             for (int c = 0;c < 3;c++) {
-                if (board[r][c] == k[i]) { count[0]++; } //count horizontally
-                if (board[c][r] == k[i]) { count[1]++; } //count vertically
-                if (r == c && board[r][c] == k[i]) { count[2]++; } //count along - diagonal
-                if (r + c == 2 && board[r][c] == k[i]) { count[3]++; }  //count along + diagonal
-                if (board[r][c] == emptyspot)HasEmptySpot = 1;
+                if (board[r][c] == player[i]) { count[0]++; } //count horizontally
+                if (board[c][r] == player[i]) { count[1]++; } //count vertically
+                if (r == c && board[r][c] == player[i]) { count[2]++; } //count along - diagonal
+                if (r + c == 2 && board[r][c] == player[i]) { count[3]++; }  //count along + diagonal
+                if (board[r][c] == null)HasEmptySpot = 1;
             }
-            for (int m = 0;m < 4;m++) { if (count[m] == 3)return k[i]; }//return winner if any
+            for (int m = 0;m < 4;m++) { if (count[m] == 3)return player[i]; }//return winner if any
             count[0] = 0;count[1] = 0; //reset only for orthogonal directions
         }
     }
@@ -64,111 +67,73 @@ void UpdateBoard(bool turn, int row, int col) {
     // row n has index n-1 and column k has index k-1
     board[row - 1][col - 1] = (turn == 0) ? o : x;
 }
-bool IsValid(int row, int col) {
-    if (row > 0 && row < 4 && col>0 && col < 4 && board[row - 1][col - 1] == emptyspot)return 1;
-    return 0;
-}
-int minimax(int row, int col, vector<vector<char>> b, int depth, bool maximizingplayer) {
-    b[row][col] = (maximizingplayer == 1) ? x : o;
-    //(row, col) = previous move
 
+int minimax(int depth, bool maximizingplayer) {
     char result = GameState();
-
     //if game over
-    if (result == 'X')return 100; //our AI, the maximising player
-    if (result == 'O')return -100;
-    if (result == 'd')return (maximizingplayer == 1) ? 50 : -50;
+    if (result == x)return 100; 
+    if (result == o)return -100;
+    if (result == 'd')return 0;
 
-    if (depth == 0) { //evaluate move
-        //first if able to stop opponent from winning, good
-        //second, a move is good if player becomes 1 move away from win.
-        char k = (maximizingplayer == 1) ? x : o;
-        int empty[4] = { 0,0,0,0 };
-        int count[4] = { 0,0,0,0 }; //count along each direction
+    int MaxEval = -1000; //max score for maximising player
+    int MinEval = 1000; //min score for minimising player
 
-        for (int r = 0;r < 3;r++) {
-            for (int c = 0;c < 3;c++) {
-
-                if (r == row || c == col) {
-                    if (b[r][c] == k) { count[0]++; } //count horizontally
-                    if (b[c][r] == k) { count[1]++; } //count vertically
-                    if (r == c && b[r][c] == k) { count[2]++; } //count along - diagonal
-                    if (r + c == 2 && b[r][c] == k) { count[3]++; }  //count along + diagonal
+    for (int i = 0;i < 3;i++) {
+        for (int j = 0;j < 3;j++) {
+            if (board[i][j] == null) {
+                board[i][j] = maximizingplayer? x:o;
+                if (maximizingplayer) {
+                       MaxEval = max(MaxEval, minimax(depth+1,0) -depth);
                 }
-
-            }
-            for (int m = 0;m < 4;m++) { if (count[m] == 2 && empty[m] == 1)return (maximizingplayer == 1) ? 75 : -75;}
-            count[0] = 0;count[1] = 0; //reset only for orthogonal directions
-        }
-        return (maximizingplayer == 1) ? 25 : -25; //neutral move
-
-    }
-
-
-    if (maximizingplayer) {
-        int MaxEval = INT_MIN;
-
-        for (int i = 0;i < 3;i++) {
-            for (int j = 0;j < 3;j++) {
-                if (b[i][j] == emptyspot) {
-                    MaxEval = max(MaxEval, minimax(i, j, b, depth - 1, 0));
+                else {
+                      MinEval = min(MinEval, minimax(depth+1, 1) + depth);
                 }
+                board[i][j] = null;
             }
         }
-        return MaxEval;
-
     }
-    else {
-        int MinEval = INT_MAX;
-
-        for (int i = 0;i < 3;i++) {
-            for (int j = 0;j < 3;j++) {
-                if (b[i][j] == emptyspot) {
-                    MinEval = min(MinEval, minimax(i, j, b, depth - 1, 1));
-                }
-            }
-        }
-        return MinEval;
-    }
+    return maximizingplayer==1 ? MaxEval : MinEval;
 }
 
 void AImove() {
-    int p=-1, q=-1; //best move is to play at (p,q)
+    int p = -1, q = -1; //best move is to play at (p,q)
     int MaxEval = INT_MIN;
 
     for (int i = 0;i < 3;i++) {
         for (int j = 0;j < 3;j++) {
-            if (board[i][j] == emptyspot) {//for each possible move
-                int eval = minimax(i, j, board, 4, 1); //evaluate move 
+            if (board[i][j] == null) {//for each possible move
+
+                board[i][j] = x;
+                int eval = minimax(0,0); 
                 if (eval > MaxEval) {//new best move discovered
                     p = i; q = j;
                     MaxEval = eval;
                 }
+                board[i][j] = null;
+
             }
         }
     }
-    //play best move
-    for (int i = 0;i < 100000000;i++);
     board[p][q] = x;
 }
 
 int main()
 {
     bool vsAI = 1;
-    bool turnX = 1;// 0 - player o  1 - player x
+    bool turnX = 1; // 0 = O first ;  1 = X first
     char state = ongoing;
     int row, col;
-
-    //cout << "vs AI? 1 = Yes \n";
-    //cin >> vsAI;
-    //cout << "Who starts? 1-x or 0-o  ";
-    //cin >> turnX;
+    cout << "vs AI? 1 = Yes \n";
+    cin >> vsAI;
+    cout << "Who starts? 1-x or 0-o  ";
+    cin >> turnX;
 
     if (vsAI) {
+
         while (state == ongoing) {
             OutputBoard();
             if (turnX == 0) {
-                cout << "O's turn" << endl;
+                cout << "Your turn" << endl;
                 cin >> row >> col;
 
                 while (IsValid(row, col) == 0) {
@@ -204,5 +169,5 @@ int main()
     }
 
     OutputBoard();
-    cout << ((state == o) ? "Player O has won!" : ((state == x) ? "Player X has won!" : "Draw")) << endl;
+    cout << ((state == o) ? "Player O has won!" : ((state == x) ? "Player X has won!" : "Draw")) << "\n";
 }
